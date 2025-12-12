5e30dfe45ea992188eb2401b9d273957c12ac878
Benjamin <benjamin.grassart@outlook.fr>
2025-12-12 17:22:40 +0100

    feat(pdf): pdf FW
    Enable to read & translate to text
    the pdf in dedicated dir.

--- src/pdf_reader.py
+++ src/pdf_reader.py
@@ -1,10 +1,8 @@
+import glob
 import pypdf
+from pathlib import Path
+from src.config import PDF_DIR, TXT_DIR
 
-CODE_DU_TRAVAIL_NAME = "code_du_travail.pdf"
-CODE_DU_TRAVAIL_PATH = "./text_sources/" + CODE_DU_TRAVAIL_NAME
-
-SYNTEC_CONVENTION_NAME = "convention_collective_syntec.pdf"
-SYNTEC_CONVENTION_PATH = "./text_sources/" + SYNTEC_CONVENTION_NAME
 
 def read_pdf(file_path: str) -> str:
     """Read a PDF file and extract text content."""
@@ -14,10 +12,35 @@ def read_pdf(file_path: str) -> str:
         text += page.extract_text() or ""
     return text
 
+def _write_text_file(content: str, pdf_file_name: str) -> None:
+    file_destination = TXT_DIR / pdf_file_name
+
+    with open(f'{file_destination}.txt', 'w', encoding='utf-8') as fichier:
+        fichier.write(content)
+        print(f'Document "{pdf_file_name}" has been written as txt and contain {len(content)} characters.')
+
+def _read_pdf_and_write_text_file(pdf_file: Path) -> None:
+    pdf_file_name = pdf_file.name
+    content = read_pdf(pdf_file.as_posix())
+    _write_text_file(content, pdf_file_name)
+    
+
+def fetch_pdfs_and_save_as_raw(dir: Path) -> None:
+    print(f"✓ Fetching documents from dir {dir}")
+    pdf_files = glob.glob(dir.as_posix() + "/*.pdf")
+
+    print(f'Found following pdf files: {pdf_files}')
+
+    for pdf_file in pdf_files:
+        _read_pdf_and_write_text_file(Path(pdf_file))
+
+def fetch_pdf_and_save_as_raw(pdf_file: Path) -> None:
+    print(f"✓ Fetching file {pdf_file}")
+    _read_pdf_and_write_text_file(pdf_file)
+
 
 if __name__ == "__main__":
-    code_du_travail_test = read_pdf(CODE_DU_TRAVAIL_PATH)
-    print(f'La taille totale du code_du_travail_est: {len(code_du_travail_test)}')
-
-    convention_syntec_test = read_pdf(SYNTEC_CONVENTION_PATH)
-    print(f'La taille totale de la convention_syntec_est: {len(convention_syntec_test)}')
+    print("Running pdf discovery and conversion to txt...")
+    print(f"fetching pdfs from PDF_DIR: {PDF_DIR}")
+    fetch_pdfs_and_save_as_raw(PDF_DIR)
+    # fetch_pdf_and_save_as_raw(CODE_DU_TRAVAIL_PATH)
