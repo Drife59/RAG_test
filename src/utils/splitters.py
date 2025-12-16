31ecd515d8698676f0242e71923a6db91f1cc1f2
Benjamin <benjamin.grassart@outlook.fr>
2025-12-16 17:12:15 +0100

    feat(ingest): POC each article as a Doc

--- src/utils/splitters.py
+++ src/utils/splitters.py
@@ -1,8 +1,11 @@
+import os
 from langchain_text_splitters import TextSplitter
 from langchain_core.documents import Document
 from src.config import TXT_DIR
+from pathlib import Path
 
 
+# TODO: improve name
 class CustomTextSplitter(TextSplitter):
     def __init__(self, separator: str = "[START]"):
         super().__init__()
@@ -24,8 +27,31 @@ class CustomTextSplitter(TextSplitter):
         chunks = self.split_text(text)
         return [Document(page_content=chunk) for chunk in chunks]
     
+def get_each_article_as_unique_doc(splitter: TextSplitter, dir: Path) -> list[Document]:
+    file_names = os.listdir(dir.as_posix())
+    print("Number of marked files:", len(file_names))
+
+    docs = []
+    for file_name in file_names:
+        with open(dir / Path(file_name), 'r', encoding='utf-8') as f:
+            tagged_text = f.read()
+            articles = splitter.split_text(tagged_text)
+
+            for article in articles:
+                docs.append(Document(page_content=article, metadata={"source": file_name}))
+    
+    return docs
+
 if __name__ == "__main__":
-    test_file_path = TXT_DIR / "marked_chunks/code_du_travail_part_1_marked.txt"
+    taggedfile_path = TXT_DIR / "tagged_chunks"
+    splitter = CustomTextSplitter()
+    docs = get_each_article_as_unique_doc(splitter, taggedfile_path)
+
+    print(f"Il y a {len(docs)} articles dans le code du travail.")
+
+
+def old_main():
+    test_file_path = TXT_DIR / "tagged_chunks/code_du_travail_part_1_marked.txt"
 
     with open(test_file_path.as_posix(), 'r', encoding='utf-8') as f:
         text = f.read()
