f7373ecb95ebc39a03d0f9e6a95bed1476847101
Benjamin <benjamin.grassart@outlook.fr>
2025-12-22 16:55:44 +0100

    feat(preprocessing): New TU on article_part_10

---
+++ src/preprocessing/article_part/articles_part_10.py
@@ -0,0 +1,195 @@
+ID_ARTICLE_L1225_24 = "Article L1225-24"
+CONTENT_ARTICLE_L1225_24 = """
+Le congé de maternité entraîne la suspension du contrat de travail. La salariée avertit l'employeur du motif de
+son absence et de la date à laquelle elle entend y mettre fin.
+ 
+La durée de ce congé est assimilée à une période de travail effectif pour la détermination des droits que la
+salariée tient de son ancienneté.
+"""
+
+ID_ARTICLE_L1225_25 = "Article L1225-25"
+CONTENT_ARTICLE_L1225_25 = """
+A l'issue du congé de maternité, la salariée retrouve son précédent emploi ou un emploi similaire assorti
+d'une rémunération au moins équivalente.
+"""
+
+ID_ARTICLE_L1225_26 = "Article L1225-26"
+CONTENT_ARTICLE_L1225_26 = """
+En l'absence d'accord collectif de branche ou d'entreprise déterminant des garanties d'évolution de la
+rémunération des salariées pendant le congé de maternité et à la suite de ce congé au moins aussi favorables
+que celles mentionnées dans le présent article, cette rémunération, au sens de l'article L. 3221-3, est majorée,
+à la suite de ce congé, des augmentations générales ainsi que de la moyenne des augmentations individuelles
+perçues pendant la durée de ce congé par les salariés relevant de la même catégorie professionnelle ou, à
+défaut, de la moyenne des augmentations individuelles dans l'entreprise.
+ 
+Cette règle n'est pas applicable aux accords collectifs de branche ou d'entreprise conclus antérieurement à
+l'entrée en vigueur de la loi n° 2006-340 du 23 mars 2006 relative à l'égalité salariale entre les femmes et les
+hommes.
+"""
+
+ID_ARTICLE_L1225_27 = "Article L1225-27"
+CONTENT_ARTICLE_L1225_27 = """
+La salariée qui reprend son activité à l'issue d'un congé de maternité a droit à l'entretien professionnel
+mentionné au I de l'article L. 6315-1.
+"""
+
+ID_ARTICLE_L1225_28 = "Article L1225-28"
+CONTENT_ARTICLE_L1225_28 = """
+En cas de décès de la mère au cours de la période d'indemnisation définie au premier alinéa de l'article L.
+331-6 du code de la sécurité sociale, le père peut suspendre son contrat de travail pendant une période au
+plus égale à la durée d'indemnisation restant à courir, définie au même premier alinéa, le cas échéant reportée
+en application du deuxième alinéa du même article.
+
+L'intéressé avertit son employeur du motif de son absence et de la date à laquelle il entend mettre fin à la
+suspension de son contrat de travail. Le père bénéficie alors de la protection contre le licenciement prévue
+aux articles L. 1225-4 et L. 1225-5.
+
+Lorsque le père de l'enfant n'exerce pas son droit, le bénéfice de celui-ci est accordé au conjoint salarié de la
+mère ou à la personne salariée liée à elle par un pacte civil de solidarité ou vivant maritalement avec elle.
+"""
+
+ID_ARTICLE_L1225_29 = "Article L1225-29"
+CONTENT_ARTICLE_L1225_29 = """
+Il est interdit d'employer la salariée pendant une période de huit semaines au total avant et après son
+accouchement.
+ 
+Il est interdit d'employer la salariée dans les six semaines qui suivent son accouchement.
+"""
+
+ID_ARTICLE_L1225_30 = "Article L1225-30"
+CONTENT_ARTICLE_L1225_30 = """
+Pendant une année à compter du jour de la naissance, la salariée allaitant son enfant dispose à cet effet d'une
+heure par jour durant les heures de travail.
+"""
+
+ID_ARTICLE_L1225_31 = "Article L1225-31"
+CONTENT_ARTICLE_L1225_31 = """
+La salariée peut allaiter son enfant dans l'établissement.
+"""
+
+ID_ARTICLE_L1225_32 = "Article L1225-32"
+CONTENT_ARTICLE_L1225_32 = """
+Tout employeur employant plus de cent salariées peut être mis en demeure d'installer dans son établissement
+ou à proximité des locaux dédiés à l'allaitement.
+"""
+
+ID_ARTICLE_L1225_33 = "Article L1225-33"
+CONTENT_ARTICLE_L1225_33 = """
+Un décret en Conseil d'Etat détermine, suivant l'importance et la nature des établissements, les conditions
+d'application de la présente sous-section.
+"""
+
+ID_ARTICLE_L1225_34 = "Article L1225-34"
+CONTENT_ARTICLE_L1225_34 = """
+La salariée en état de grossesse médicalement constaté peut rompre son contrat de travail sans préavis et sans
+devoir d'indemnité de rupture.
+"""
+
+ID_ARTICLE_L1225_35 = "Article L1225-35"
+CONTENT_ARTICLE_L1225_35 = """
+Après la naissance de l'enfant, le père salarié ainsi que, le cas échéant, le conjoint ou concubin salarié de la
+mère ou la personne salariée liée à elle par un pacte civil de solidarité bénéficient d'un congé de paternité et
+d'accueil de l'enfant de vingt-cinq jours calendaires ou de trente-deux jours calendaires en cas de naissances
+multiples.
+ 
+Le congé de paternité et d'accueil de l'enfant entraîne la suspension du contrat de travail.
+ 
+Ce congé est composé d'une période de quatre jours calendaires consécutifs, faisant immédiatement suite au
+congé de naissance mentionné au 3° de l'article L. 3142-1, et d'une période de vingt et un jours calendaires,
+portée à vingt-huit jours calendaires en cas de naissances multiples.  
+Le délai de prévenance de l'employeur quant à la date prévisionnelle de l'accouchement et aux dates de prise
+du congé et à la durée de la ou des périodes de congés, le délai dans lequel les jours de congé doivent être
+pris ainsi que les modalités de fractionnement de la période de congé de vingt et un jours et de vingt-huit
+jours sont fixés par décret. Le délai de prévenance relatif à la date prévisionnelle de l'accouchement et celui
+relatif aux dates de prise du ou des congés de la seconde période de vingt et un jours ou de vingt-huit jours
+ainsi qu'à la durée de ces congés doivent être compris entre quinze jours et deux mois.
+ 
+ 
+Par dérogation aux quatre premiers alinéas, lorsque l'état de santé de l'enfant nécessite son hospitalisation
+immédiate après la naissance dans une unité de soins spécialisée définie par arrêté des ministres chargés de
+la santé et de la sécurité sociale, la période de congé de quatre jours consécutifs mentionnée au troisième
+alinéa est prolongée de droit, à la demande du salarié, pendant la période d'hospitalisation, dans la limite
+d'une durée maximale déterminée par décret.
+"""
+
+ID_ARTICLE_L1225_35_1 = "Article L1225-35-1"
+CONTENT_ARTICLE_L1225_35_1 = """
+Il est interdit d'employer le salarié pendant le congé mentionné au 3° de l'article L. 3142-1 et pendant la
+période de congé de paternité et d'accueil de l'enfant de quatre jours mentionnée au troisième alinéa de
+l'article L. 1225-35, à l'exception de sa prolongation éventuelle mentionnée au dernier alinéa du même article
+L. 1225-35 et sans qu'y fasse, le cas échéant, obstacle le non-respect par le salarié du délai de prévenance
+mentionné à l'avant-dernier alinéa dudit article L. 1225-35.  
+Si la naissance de l'enfant intervient alors que le salarié a pris des congés payés ou un congé pour
+évènements familiaux, l'interdiction d'emploi débute à l'issue de cette période de congés.  
+L'interdiction d'emploi ne s'applique pas pour le congé mentionné au troisième alinéa de l'article L. 1225-35
+lorsque le salarié ne peut pas bénéficier des indemnités et allocations versées dans les conditions prévues à
+l'article L. 331-8 du code de la sécurité sociale ou par d'autres dispositions législatives ou réglementaires.
+"""
+
+ID_ARTICLE_L1225_35_2 = "Article L1225-35-2"
+CONTENT_ARTICLE_L1225_35_2 = """
+La durée du congé de paternité et d'accueil de l'enfant est assimilée à une période de travail effectif pour la
+détermination des droits que le salarié tient de son ancienneté.  
+Le salarié conserve le bénéfice de tous les avantages qu'il avait acquis avant le début du congé.
+"""
+
+ID_ARTICLE_L1225_36 = "Article L1225-36"
+CONTENT_ARTICLE_L1225_36 = """
+A l'issue du congé de paternité et d'accueil de l'enfant, le salarié retrouve son précédent emploi ou un emploi
+similaire assorti d'une rémunération au moins équivalente.
+"""
+
+ID_ARTICLE_L1225_37 = "Article L1225-37"
+CONTENT_ARTICLE_L1225_37 = """
+Le salarié à qui l'autorité administrative ou tout organisme désigné par voie réglementaire confie un enfant
+en vue de son adoption a le droit de bénéficier d'un congé d'adoption d'une durée de seize semaines au plus,
+pris dans un délai et fractionné selon des modalités déterminées par décret.
+ 
+Le congé d'adoption est porté à :
+ 
+1° Dix-huit semaines lorsque l'adoption porte à trois ou plus le nombre d'enfants dont le salarié ou le foyer
+assume la charge ;
+ 
+2° Vingt-deux semaines en cas d'adoptions multiples.
+"""
+
+ID_ARTICLE_L1225_38 = "Article L1225-38"
+CONTENT_ARTICLE_L1225_38 = """
+Le congé d'adoption suspend le contrat de travail.
+ 
+Pendant la suspension, les parents salariés bénéficient de la protection contre le licenciement prévue aux
+articles L. 1225-4 et L. 1225-5. L'application de ces articles ne fait pas obstacle à l'échéance du contrat de
+travail à durée déterminée.
+"""
+
+ID_ARTICLE_L1225_39 = "Article L1225-39"
+CONTENT_ARTICLE_L1225_39 = """
+Le licenciement d'un salarié est annulé lorsque, dans un délai de quinze jours à compter de sa notification,
+l'intéressé envoie à son employeur, dans des conditions déterminées par voie réglementaire, une attestation
+justifiant l'arrivée à son foyer, dans un délai de quinze jours, d'un enfant placé en vue de son adoption. Cette
+attestation est délivrée par l'autorité administrative ou par l'organisme autorisé pour l'adoption qui procède au
+placement.
+ 
+Ces dispositions ne s'appliquent pas lorsque le licenciement est prononcé pour une faute grave non liée à
+l'adoption ou par impossibilité de maintenir le contrat de travail pour un motif étranger à l'adoption.
+"""
+
+ID_ARTICLE_L1225_40 = "Article L1225-40"
+CONTENT_ARTICLE_L1225_40 = """
+Lorsque la durée du congé d'adoption est répartie entre les deux parents, l'adoption d'un enfant par un couple
+de parents salariés ouvre droit à vingt-cinq jours supplémentaires de congé d'adoption ou à trente-deux jours
+en cas d'adoptions multiples.
+ 
+Le congé ainsi réparti ne peut être d'une durée supérieure, pour chaque parent, à la durée de seize semaines
+ou, le cas échéant, de dix-huit ou vingt-deux semaines prévue à l'article L. 1225-37.
+ 
+Ces deux périodes peuvent être simultanées.
+"""
+
+ID_ARTICLE_L1225_41 = "Article L1225-41"
+CONTENT_ARTICLE_L1225_41 = """
+Le salarié titulaire de l'agrément mentionné aux articles L. 225-2 et L. 225-17 du code de l'action sociale et
+des familles bénéficie du congé d'adoption lorsqu'il adopte ou accueille un enfant en vue de son adoption par
+décision de l'autorité étrangère compétente, à condition que l'enfant ait été autorisé, à ce titre, à entrer sur le
+territoire national.
+"""
--- src/preprocessing/test_article_extractor.py
+++ src/preprocessing/test_article_extractor.py
@@ -17,9 +17,30 @@ from src.preprocessing.article_part.articles_part_1 import (
     ID_ARTICLE_L1132_2, CONTENT_ARTICLE_L1132_2
 )
 
+from src.preprocessing.article_part.articles_part_10 import (
+    ID_ARTICLE_L1225_24, CONTENT_ARTICLE_L1225_24,
+    ID_ARTICLE_L1225_25, CONTENT_ARTICLE_L1225_25,
+    ID_ARTICLE_L1225_26, CONTENT_ARTICLE_L1225_26,
+    ID_ARTICLE_L1225_27, CONTENT_ARTICLE_L1225_27,
+    ID_ARTICLE_L1225_28, CONTENT_ARTICLE_L1225_28,
+    ID_ARTICLE_L1225_29, CONTENT_ARTICLE_L1225_29,
+    ID_ARTICLE_L1225_30, CONTENT_ARTICLE_L1225_30,
+    ID_ARTICLE_L1225_31, CONTENT_ARTICLE_L1225_31,
+    ID_ARTICLE_L1225_32, CONTENT_ARTICLE_L1225_32,
+    ID_ARTICLE_L1225_33, CONTENT_ARTICLE_L1225_33,
+    ID_ARTICLE_L1225_34, CONTENT_ARTICLE_L1225_34,
+    ID_ARTICLE_L1225_35, CONTENT_ARTICLE_L1225_35,
+    ID_ARTICLE_L1225_36, CONTENT_ARTICLE_L1225_36,
+    ID_ARTICLE_L1225_37, CONTENT_ARTICLE_L1225_37,
+    ID_ARTICLE_L1225_38, CONTENT_ARTICLE_L1225_38,
+    ID_ARTICLE_L1225_39, CONTENT_ARTICLE_L1225_39,
+    ID_ARTICLE_L1225_40, CONTENT_ARTICLE_L1225_40,
+    ID_ARTICLE_L1225_41, CONTENT_ARTICLE_L1225_41,
+)
+
 TEST_DIR = TXT_DIR / "raw_chunks" 
 
-MINIMUM_SIMILARITY = 0.97
+MINIMUM_SIMILARITY = 0.95
 
 def test_article_extractor_part_1():
     articles = get_articles(TEST_DIR / "code_du_travail_part_1.txt", MINISTRAL_3B)
@@ -59,5 +80,51 @@ def test_article_extractor_part_1():
     assert string_similarity(article_by_id[ID_ARTICLE_L1132_1], CONTENT_ARTICLE_L1132_1) > MINIMUM_SIMILARITY
     assert string_similarity(article_by_id[ID_ARTICLE_L1132_2], CONTENT_ARTICLE_L1132_2) > MINIMUM_SIMILARITY
 
-    
+def test_article_extractor_part_10():
+    articles = get_articles(TEST_DIR / "code_du_travail_part_10.txt", MINISTRAL_3B)
+
+    assert len(articles) == 20
+
+    article_by_id = index_article_by_id(articles)
+
+    article_ids = article_by_id.keys()
+
+    assert ID_ARTICLE_L1225_24 in article_ids
+    assert ID_ARTICLE_L1225_25 in article_ids
+    assert ID_ARTICLE_L1225_26 in article_ids
+    assert ID_ARTICLE_L1225_27 in article_ids
+    assert ID_ARTICLE_L1225_28 in article_ids
+    assert ID_ARTICLE_L1225_29 in article_ids
+    assert ID_ARTICLE_L1225_30 in article_ids    
+    assert ID_ARTICLE_L1225_31 in article_ids
+    assert ID_ARTICLE_L1225_32 in article_ids    
+    assert ID_ARTICLE_L1225_33 in article_ids
+    assert ID_ARTICLE_L1225_34 in article_ids
+    assert ID_ARTICLE_L1225_35 in article_ids
+    assert ID_ARTICLE_L1225_36 in article_ids
+    assert ID_ARTICLE_L1225_37 in article_ids
+    assert ID_ARTICLE_L1225_38 in article_ids
+    assert ID_ARTICLE_L1225_39 in article_ids
+    assert ID_ARTICLE_L1225_40 in article_ids
+    assert ID_ARTICLE_L1225_41 in article_ids
+
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_24], CONTENT_ARTICLE_L1225_24) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_25], CONTENT_ARTICLE_L1225_25) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_26], CONTENT_ARTICLE_L1225_26) > MINIMUM_SIMILARITY 
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_27], CONTENT_ARTICLE_L1225_27) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_28], CONTENT_ARTICLE_L1225_28) > MINIMUM_SIMILARITY 
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_29], CONTENT_ARTICLE_L1225_29) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_30], CONTENT_ARTICLE_L1225_30) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_31], CONTENT_ARTICLE_L1225_31) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_32], CONTENT_ARTICLE_L1225_32) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_33], CONTENT_ARTICLE_L1225_33) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_34], CONTENT_ARTICLE_L1225_34) > MINIMUM_SIMILARITY
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_35], CONTENT_ARTICLE_L1225_35) > MINIMUM_SIMILARITY 
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_36], CONTENT_ARTICLE_L1225_36) > MINIMUM_SIMILARITY 
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_37], CONTENT_ARTICLE_L1225_37) > MINIMUM_SIMILARITY 
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_38], CONTENT_ARTICLE_L1225_38) > MINIMUM_SIMILARITY 
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_39], CONTENT_ARTICLE_L1225_39) > MINIMUM_SIMILARITY 
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_40], CONTENT_ARTICLE_L1225_40) > MINIMUM_SIMILARITY 
+    assert string_similarity(article_by_id[ID_ARTICLE_L1225_41], CONTENT_ARTICLE_L1225_41) > MINIMUM_SIMILARITY  
+  
 
