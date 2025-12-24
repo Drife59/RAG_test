a58cf5f0f7f9cf7a5898562ef0cb6817071e8c11
Benjamin <benjamin.grassart@outlook.fr>
2025-12-24 16:05:20 +0100

    test(preprocessing): Parametrize TU articles ...
    Extractor

--- src/preprocessing/extractor/test_article_extractor.py
+++ src/preprocessing/extractor/test_article_extractor.py
@@ -1,6 +1,8 @@
+import pytest
 from src.config import TXT_DIR
 from src.preprocessing.utils import string_similarity
-from src.models.mistral_models import MINISTRAL_3B, frontier_mistral_client
+from src.models.mistral_models import frontier_mistral_client, MINISTRAL_3B as FRONTIER_MINISTRAL_3B
+from src.models.ollama_models import ollama_client, MINISTRAL3_3B, MISTRAL_7B, LLAMA_3_2
 from src.preprocessing.extractor.article_extractor import get_articles, index_article_by_id
 from src.preprocessing.cleaner.article_cleaner import clean_article
 from src.preprocessing.extractor.article_part.articles_part_1 import (
@@ -43,8 +45,17 @@ TEST_DIR = TXT_DIR / "test"
 
 MINIMUM_SIMILARITY = 0.95
 
-def test_article_extractor_part_1():
-    articles = get_articles(TEST_DIR / "code_du_travail_part_1.txt", frontier_mistral_client, MINISTRAL_3B)
+MODELS_TO_TEST = [
+    # This model fails
+    # (ollama_client, MINISTRAL3_3B),
+    # (ollama_client, LLAMA_3_2),
+    # This model works
+    (frontier_mistral_client, FRONTIER_MINISTRAL_3B),
+]
+
+@pytest.mark.parametrize("client, model", MODELS_TO_TEST)
+def test_article_extractor_part_1(client, model):
+    articles = get_articles(TEST_DIR / "code_du_travail_part_1.txt", client, model)
     # 13 articles in this first file, but the last one is troncated and was removed
     assert len(articles) == 12
 
@@ -81,8 +92,9 @@ def test_article_extractor_part_1():
     assert string_similarity(article_by_id[ID_ARTICLE_L1132_1], CONTENT_ARTICLE_L1132_1) > MINIMUM_SIMILARITY
     assert string_similarity(article_by_id[ID_ARTICLE_L1132_2], CONTENT_ARTICLE_L1132_2) > MINIMUM_SIMILARITY
 
-def test_article_extraction_plus_clean_part_1():
-    articles = get_articles(TEST_DIR / "code_du_travail_part_1.txt", frontier_mistral_client, MINISTRAL_3B)
+@pytest.mark.parametrize("client, model", MODELS_TO_TEST)
+def test_article_extraction_plus_clean_part_1(client, model):
+    articles = get_articles(TEST_DIR / "code_du_travail_part_1.txt", client, model)
     article_by_id = index_article_by_id(articles)
     article_ids = article_by_id.keys()
 
@@ -91,20 +103,22 @@ def test_article_extraction_plus_clean_part_1():
     assert ID_ARTICLE_L1121_1 in article_ids
     assert ID_ARTICLE_L1131_2 in article_ids
 
-    cleaned_content_l1 = clean_article(frontier_mistral_client, article_by_id[ID_ARTICLE_L1])
+    cleaned_content_l1 = clean_article(client, model, article_by_id[ID_ARTICLE_L1])
     assert string_similarity(article_by_id[ID_ARTICLE_L1], cleaned_content_l1) > MINIMUM_SIMILARITY
 
-    cleaned_content_l1111_1 = clean_article(frontier_mistral_client, article_by_id[ID_ARTICLE_L1111_1])
+    cleaned_content_l1111_1 = clean_article(client, model, article_by_id[ID_ARTICLE_L1111_1])
     assert string_similarity(article_by_id[ID_ARTICLE_L1111_1], cleaned_content_l1111_1) > MINIMUM_SIMILARITY
 
-    cleaned_content_l1121_1 = clean_article(frontier_mistral_client, article_by_id[ID_ARTICLE_L1121_1])
+    cleaned_content_l1121_1 = clean_article(client, model, article_by_id[ID_ARTICLE_L1121_1])
     assert string_similarity(article_by_id[ID_ARTICLE_L1121_1], cleaned_content_l1121_1) > MINIMUM_SIMILARITY
 
-    cleaned_content_l1131_2 = clean_article(frontier_mistral_client, article_by_id[ID_ARTICLE_L1131_2])
+    cleaned_content_l1131_2 = clean_article(client, model, article_by_id[ID_ARTICLE_L1131_2])
     assert string_similarity(article_by_id[ID_ARTICLE_L1131_2], cleaned_content_l1131_2) > MINIMUM_SIMILARITY
 
-def test_article_extractor_part_10():
-    articles = get_articles(TEST_DIR / "code_du_travail_part_10.txt", frontier_mistral_client, MINISTRAL_3B)
+
+@pytest.mark.parametrize("client, model", MODELS_TO_TEST)
+def test_article_extractor_part_10(client, model):
+    articles = get_articles(TEST_DIR / "code_du_travail_part_10.txt", client, model)
 
     assert len(articles) == 20
 
