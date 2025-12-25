f2efb0c0b0c1199ed707e99bd0762c1cabc62971
Benjamin <benjamin.grassart@outlook.fr>
2025-12-25 17:39:22 +0100

    feat(preproc): improve extractor message

--- src/preprocessing/extractor/article_extractor.py
+++ src/preprocessing/extractor/article_extractor.py
@@ -30,6 +30,9 @@ user_message = """
 
     Voici des exemples d'identifiants d'articles:
     "Article L1111-2", "Article L1251-46", "Article D1221-25", "Article R1221-36", "Article D1226-8", "Article R4451-17"
+    Un identifiant d'article comporte obligatoirement le mot "Article".
+    N'invente pas d'identifiant d'article si tu n'en trouve pas.
+    
     Contexte:
     {context}
 """
