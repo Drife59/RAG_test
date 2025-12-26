16164365cca41df4bee887e3d508c6c44e08fce7
Benjamin <benjamin.grassart@outlook.fr>
2025-12-26 16:38:32 +0100

    refacto(ingest): refacto ingest db

--- src/ingest_to_sql_db.py
+++ src/ingest_to_sql_db.py
@@ -24,14 +24,18 @@ async def get_file_names_to_process(dir: Path) -> list[str]:
     print(f"{len(filtered_file_names)} files will be processed.\n")
     return filtered_file_names
 
-
-def get_cleaned_articles_from_chunks(file_path: Path) -> dict[str, SourcedArticle]:
-    print(f"\nProcessing {file_path}...")
-    sourced_articles = get_sourced_articles(file_path, extractor_client, FRONTIER_MINISTRAL_3B)
-    print(f"{len(sourced_articles)} articles extracted from {file_path}.", flush=True)
-    articles_by_id = index_article_by_id(sourced_articles)
-
-    return articles_by_id
+async def save_article(article: SourcedArticle) -> None:
+    new_article = Article(
+        id=article.id,
+        content=article.content,
+        source=article.source
+    )
+    try:
+        # I prefer not to insert in bulk, to see any single article failure
+        await Article.insert(new_article)
+    except Exception as e:
+        print("could not save article", new_article)
+        print("Error:", e)
 
 async def save_articles(articles_by_id: dict[str, SourcedArticle]) -> None:
     db_articles_ids = [article.id for article in await Article.objects()]
@@ -40,38 +44,36 @@ async def save_articles(articles_by_id: dict[str, SourcedArticle]) -> None:
     print(f"Saving {len(articles_to_save)} new articles.")
 
     for article in tqdm(articles_to_save):
-        new_article = Article(
-            id=article.id,
-            content=article.content,
-            source=article.source
-        )
-        try:
-            # I prefer not to insert in bulk, to see any single article failure
-            await Article.insert(new_article)
-        except Exception as e:
-            print("could not save article", new_article)
-            print("Error:", e)
+        await save_article(article)
+
+def get_articles_from_chunks(file_path: Path) -> dict[str, SourcedArticle]:
+    print(f"\nProcessing {file_path}...")
+    sourced_articles = get_sourced_articles(file_path, extractor_client, FRONTIER_MINISTRAL_3B)
+    print(f"{len(sourced_articles)} articles extracted from {file_path}.", flush=True)
+    article_by_id = index_article_by_id(sourced_articles)
+
+    return article_by_id
 
 async def fetch_articles_and_save_to_db(dir: Path) -> dict[str, SourcedArticle]:
     # Avoid processing already processed files
     file_names = await get_file_names_to_process(dir)
 
-    articles_by_id: dict[str, SourcedArticle] = {}
+    article_by_id: dict[str, SourcedArticle] = {}
     for file_name in file_names:
         file_path = Path(dir.as_posix() + "/" + file_name)
-        current_file_articles_by_id = get_cleaned_articles_from_chunks(file_path)
-        await save_articles(current_file_articles_by_id)
-        articles_by_id |= current_file_articles_by_id
+        current_file_article_by_id = get_articles_from_chunks(file_path)
+        await save_articles(current_file_article_by_id)
+        article_by_id |= current_file_article_by_id
 
-    return articles_by_id
+    return article_by_id
 
 
 async def main():
     chunk_dir = TXT_DIR / "chunks"
 
-    articles_by_id = await fetch_articles_and_save_to_db(chunk_dir)
+    article_by_id = await fetch_articles_and_save_to_db(chunk_dir)
 
-    print(f'{len(articles_by_id)} articles saved to DB.')
+    print(f'{len(article_by_id)} articles saved to DB.')
     print("Ingestion from file to SQL complete.")
 
 
