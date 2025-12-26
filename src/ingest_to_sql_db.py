b7a864fdeaaddbf57e94659f1a77915338389eb4
Benjamin <benjamin.grassart@outlook.fr>
2025-12-26 16:24:08 +0100

    feat(ingest): ingest in two files
    And delete cleaning process

--- src/ingest_to_sql_db.py
+++ src/ingest_to_sql_db.py
@@ -1,37 +1,17 @@
 import os
 from pathlib import Path
 
-from langchain_core.documents import Document
 from tqdm import tqdm  # type: ignore
 
 from src.config import TXT_DIR
-from src.embeddings.embed import create_embeddings
 from src.models.mistral_models import MINISTRAL_3B as FRONTIER_MINISTRAL_3B
 from src.models.mistral_models import frontier_mistral_client
-from src.models.ollama_models import MINISTRAL3_3B, ollama_client
-from src.preprocessing.cleaner.article_cleaner import clean_article_content
 from src.preprocessing.extractor.article_extractor import SourcedArticle, get_sourced_articles, index_article_by_id
 from src.rag_db.tables import Article
-
-# From cleaning article, local model actually works fine
-cleaner_client = ollama_client
-cleaner_model = MINISTRAL3_3B 
 
 extractor_client = frontier_mistral_client
 extractor_model = FRONTIER_MINISTRAL_3B
 
-
-def clean_articles(articles_by_id: dict[str, SourcedArticle]) -> dict[str, SourcedArticle]:
-    print("Start cleaning articles...")
-
-    cleaned_articles_by_id: dict[str, SourcedArticle] = {}
-    for article_id, sourced_article in tqdm(articles_by_id.items()):
-        cleaned_content = clean_article_content(cleaner_client, cleaner_model, sourced_article.content)
-        cleaned_articles_by_id[article_id] = sourced_article
-        cleaned_articles_by_id[article_id].content = cleaned_content
-
-    print(f"{len(cleaned_articles_by_id)} articles cleaned.")
-    return cleaned_articles_by_id
 
 async def get_file_names_to_process(dir: Path) -> list[str]:
     """Check in db filename already processed and return list of file names to process."""
@@ -50,7 +30,6 @@ def get_cleaned_articles_from_chunks(file_path: Path) -> dict[str, SourcedArticl
     sourced_articles = get_sourced_articles(file_path, extractor_client, FRONTIER_MINISTRAL_3B)
     print(f"{len(sourced_articles)} articles extracted from {file_path}.", flush=True)
     articles_by_id = index_article_by_id(sourced_articles)
-    # cleaned_articles_by_id = clean_articles(articles_by_id)
 
     return articles_by_id
 
@@ -60,7 +39,7 @@ async def save_articles(articles_by_id: dict[str, SourcedArticle]) -> None:
     articles_to_save = [article for article in articles_by_id.values() if article.id not in db_articles_ids]
     print(f"Saving {len(articles_to_save)} new articles.")
 
-    for article in articles_to_save:
+    for article in tqdm(articles_to_save):
         new_article = Article(
             id=article.id,
             content=article.content,
@@ -73,7 +52,7 @@ async def save_articles(articles_by_id: dict[str, SourcedArticle]) -> None:
             print("could not save article", new_article)
             print("Error:", e)
 
-async def get_articles_from_dir(dir: Path) -> dict[str, SourcedArticle]:
+async def fetch_articles_and_save_to_db(dir: Path) -> dict[str, SourcedArticle]:
     # Avoid processing already processed files
     file_names = await get_file_names_to_process(dir)
 
@@ -86,30 +65,14 @@ async def get_articles_from_dir(dir: Path) -> dict[str, SourcedArticle]:
 
     return articles_by_id
 
-async def get_each_article_as_unique_doc(dir: Path) -> list[Document]:
-    articles_by_id = await get_articles_from_dir(dir)
-
-    documents = []
-    for article_id, article in articles_by_id.items():
-        documents.append(
-            Document(
-                page_content=article.content, 
-                metadata={"article_id": article_id, "source": article.source}
-            )
-        )
-
-    print(f'Found: {len(documents)} from processed files.')
-    return documents
-
 
 async def main():
-    from src.embeddings.embedding_models import current_embedding_model
     chunk_dir = TXT_DIR / "chunks"
 
-    documents = await get_each_article_as_unique_doc(chunk_dir)
+    articles_by_id = await fetch_articles_and_save_to_db(chunk_dir)
 
-    create_embeddings(documents, current_embedding_model)
-    print("Ingestion complete")
+    print(f'{len(articles_by_id)} articles saved to DB.')
+    print("Ingestion from file to SQL complete.")
 
 
 if __name__ == "__main__":
---
+++ src/ingest_to_vector_db.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+
+from langchain_core.documents import Document
+
+from src.config import TXT_DIR
+from src.embeddings.embed import create_embeddings
+from src.models.mistral_models import MINISTRAL_3B as FRONTIER_MINISTRAL_3B
+from src.models.mistral_models import frontier_mistral_client
+from src.rag_db.tables import Article
+
+ingest_client = frontier_mistral_client
+ingest_model = FRONTIER_MINISTRAL_3B
+
+
+async def get_each_article_as_unique_doc(dir: Path) -> list[Document]:
+    # TODO: FINISH. BTW: does this work ?
+    articles = await Article.objects()
+
+    documents = []
+    for article in articles:
+        documents.append(
+            Document(
+                page_content=article.content, 
+                metadata={"article_id": article.id, "source": article.source}
+            )
+        )
+
+    print(f'Found: {len(documents)} from DB.')
+    return documents
+
+
+async def main():
+    from src.embeddings.embedding_models import current_embedding_model
+    chunk_dir = TXT_DIR / "chunks"
+
+    documents = await get_each_article_as_unique_doc(chunk_dir)
+
+    create_embeddings(documents, current_embedding_model)
+    print("Ingestion for SQL DB complete")
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
